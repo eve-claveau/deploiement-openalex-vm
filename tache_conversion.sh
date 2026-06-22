2f6eb5e1f1bbf67d348f09efa1c1d1162632c0b3
eve-claveau <eveclaveau13@gmail.com>
2026-06-22 14:37:16 -0400

    Add files via upload

---
+++ tache_conversion.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#SBATCH --account=def-yacineb
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --output=conversion_%j.out       
+
+module load gcc arrow
+
+# Activer l'environnement virtuel
+source data_env/bin/activate
+
+# Faire rouler le script de conversion
+python flatten-openalex-parquet.py
